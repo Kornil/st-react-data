82f62dd1f54748a56ea1264fc7713e4064cd3634
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-11-29 20:24:01 +0100

    Update tests

--- src/client/containers/Home/Home.test.tsx
+++ src/client/containers/Home/Home.test.tsx
@@ -1,9 +1,47 @@
+import { shallow } from "enzyme";
+import fetchMock from "fetch-mock";
 import React from "react";
 import { render, unmountComponentAtNode } from "react-dom";
+
+import { ErrorPage } from "../";
 import Home from "./";
 
-it("renders without crashing", () => {
-  const div = document.createElement("div");
-  render(<Home />, div);
-  unmountComponentAtNode(div);
+// @ts-ignore
+fetchMock.config = Object.assign(fetchMock.config, { overwriteRoutes: true });
+
+describe("<Home />", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    render(<Home />, div);
+    unmountComponentAtNode(div);
+  });
+
+  it("renders ErrorPage if fetch fails", async () => {
+    fetchMock.get(`/bandwidth`, { throws: new Error() });
+    const wrapper = shallow(<Home />);
+
+    const instance = wrapper.instance() as Home;
+
+    try {
+      await instance.fetchBandwidthData();
+    } catch (error) {
+      // something
+    }
+
+    expect(wrapper.find(ErrorPage)).toHaveLength(1);
+  });
+
+  it("renders corecctly if fetch success", async () => {
+    fetchMock.get(
+      `/bandwidth`,
+      JSON.stringify({ cdn: [12314, 1234], p2p: [123, 4124] })
+    );
+    const wrapper = shallow(<Home />);
+
+    const instance = wrapper.instance() as Home;
+
+    await instance.fetchBandwidthData();
+
+    expect(wrapper.find("h3")).toHaveLength(1);
+  });
 });
