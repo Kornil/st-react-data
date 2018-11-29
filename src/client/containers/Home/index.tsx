66d4c65c9378cb6cf531eaf2e476be202409e329
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-11-29 18:56:36 +0100

    Switch Home to Component

--- src/client/containers/Home/index.tsx
+++ src/client/containers/Home/index.tsx
@@ -1,10 +1,14 @@
-import React from "react";
+import React, { Component } from "react";
 
-const Home = () => (
-  <div>
-    <h2>Home Page</h2>
-    <p>Hello</p>
-  </div>
-);
+class Home extends Component {
+  render() {
+    return (
+      <div>
+        <h2>Home Page</h2>
+        <p>Hello</p>
+      </div>
+    );
+  }
+}
 
 export default Home;
