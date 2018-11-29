9debff6b3231c6444d12ae07a97c74262225deb8
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-11-29 20:23:53 +0100

    Rewrite Home container to handle /bandwidth fetch

--- src/client/containers/Home/index.tsx
+++ src/client/containers/Home/index.tsx
@@ -1,13 +1,56 @@
 import React, { Component } from "react";
 
-class Home extends Component {
+import { ErrorPage, LoadingPage } from "../";
+
+interface HomeState {
+  status: "loading" | "error" | "success";
+  bandwidthData: {
+    cdn: number[];
+    p2p: number[];
+  } | null;
+}
+
+class Home extends Component<{}, HomeState> {
+  state: HomeState = {
+    bandwidthData: null,
+    status: "loading"
+  };
+
+  async componentDidMount() {
+    this.fetchBandwidthData();
+  }
+
+  // stop async operations to avoid data leaks
+  componentWillUnmount() {
+    this.setState = () => undefined;
+  }
+
+  fetchBandwidthData = async () => {
+    try {
+      const response: Response = await fetch("/bandwidth");
+      const bandwidthData = await response.json();
+
+      this.setState({
+        bandwidthData,
+        status: "success"
+      });
+    } catch (error) {
+      this.setState({
+        status: "error"
+      });
+    }
+  };
+
   render() {
-    return (
-      <div>
-        <h2>Home Page</h2>
-        <p>Hello</p>
-      </div>
-    );
+    const { status } = this.state;
+      switch(status) {
+        case "loading":
+          return <LoadingPage />
+        case "error":
+          return <ErrorPage />
+        case "success":
+          return <h3>hello</h3>
+      }
   }
 }
 
