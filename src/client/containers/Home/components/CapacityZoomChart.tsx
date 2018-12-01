450b77a73dc73d3f3385c01aca6d85e7f34a2aed
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-12-01 21:22:58 +0100

    Style Zoom Chart

--- src/client/containers/Home/components/CapacityZoomChart.tsx
+++ src/client/containers/Home/components/CapacityZoomChart.tsx
@@ -28,32 +28,34 @@ const CapacityZoomChart = ({
   selectedDomain,
   onBrush
 }: CapacityZoomChartProps) => (
-  <VictoryGroup
-    padding={{ top: 0, left: 50, right: 50, bottom: 30 }}
-    width={1200}
-    height={100}
-    scale={{ x: "time" }}
-    containerComponent={
-      <VictoryBrushContainer
-        brushDimension="x"
-        brushDomain={selectedDomain}
-        onBrushDomainChange={onBrush}
+  <div className="capacity-zoom-graph">
+    <VictoryGroup
+      padding={{ top: 0, left: 50, right: 50, bottom: 30 }}
+      width={1200}
+      height={100}
+      scale={{ x: "time" }}
+      containerComponent={
+        <VictoryBrushContainer
+          brushDimension="x"
+          brushDomain={selectedDomain}
+          onBrushDomainChange={onBrush}
+        />
+      }
+    >
+      <VictoryArea
+        data={p2p}
+        x={x}
+        y={y}
+        style={{ data: { fill: "#4FBCF2" } }}
       />
-    }
-  >
-    <VictoryArea
-      data={p2p}
-      x={x}
-      y={y}
-      style={{ data: { fill: "#4FBCF2" } }}
-    />
-    <VictoryArea
-      data={cdn}
-      x={x}
-      y={y}
-      style={{ data: { fill: "#B2125C" } }}
-    />
-  </VictoryGroup>
+      <VictoryArea
+        data={cdn}
+        x={x}
+        y={y}
+        style={{ data: { fill: "#B2125C" } }}
+      />
+    </VictoryGroup>
+  </div>
 );
 
 export default CapacityZoomChart;
