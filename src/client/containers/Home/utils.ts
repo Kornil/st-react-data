1b8d6abc40dcd02bfd35c7dbd1fb17baa8cde5fa
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-12-01 18:47:21 +0100

    Refactor utils

--- src/client/containers/Home/utils.ts
+++ src/client/containers/Home/utils.ts
@@ -13,9 +13,14 @@ const Months: string[] = [
   "Dec"
 ];
 
-interface DataInterface {
+interface BandwidthDataInterface {
   date: Date;
   gbps: number;
+}
+
+interface AudienceDataInterface {
+  date: Date;
+  audience: number;
 }
 
 type formatTimeType = (date: number) => string;
@@ -23,14 +28,28 @@ type formatTimeType = (date: number) => string;
 export const formatTime: formatTimeType = date =>
   `${new Date(date).getDate()}. ${Months[new Date(date).getMonth()]}`;
 
-type formatDataType = (data: number[][]) => DataInterface[];
+type formatBandwidthDataType = (data: number[][]) => BandwidthDataInterface[];
 
-export const formatData: formatDataType = data => {
+export const formatBandwidthData: formatBandwidthDataType = data => {
   const result = [];
   for (let i = 0; i < data.length; i += 1) {
     const newData = {
       date: new Date(data[i][0]),
       gbps: data[i][1]
+    };
+    result[i] = newData;
+  }
+  return result;
+};
+
+type formatAudienceDataType = (data: number[][]) => AudienceDataInterface[];
+
+export const formatAudienceData: formatAudienceDataType = data => {
+  const result = [];
+  for (let i = 0; i < data.length; i += 1) {
+    const newData = {
+      audience: data[i][1],
+      date: new Date(data[i][0])
     };
     result[i] = newData;
   }
@@ -44,26 +63,30 @@ export const formatBytes: formatBytesType = bytes =>
 
 type findRightDataType = (
   date: Date,
-  cdn: DataInterface[],
-  p2p: DataInterface[]
+  cdn: BandwidthDataInterface[],
+  p2p: BandwidthDataInterface[]
 ) => {
-  cdn: DataInterface | undefined;
-  p2p: DataInterface | undefined;
+  cdn: BandwidthDataInterface | undefined;
+  p2p: BandwidthDataInterface | undefined;
 };
 
 export const findRightData: findRightDataType = (date, cdn, p2p) => {
-  const cdnValue = cdn.find((value: DataInterface) => value.date.getTime() === date.getTime());
-  const p2pValue = p2p.find((value: DataInterface) => value.date.getTime() === date.getTime());
+  const cdnValue = cdn.find(
+    (value: BandwidthDataInterface) => value.date.getTime() === date.getTime()
+  );
+  const p2pValue = p2p.find(
+    (value: BandwidthDataInterface) => value.date.getTime() === date.getTime()
+  );
   return {
     cdn: cdnValue,
     p2p: p2pValue
-  }
+  };
 };
 
 type getPercentageType = (value1: number, value2: number) => string;
 
 export const getPercentage: getPercentageType = (value1, value2) => {
   const decreaseValue = value1 - value2;
-  const result = (decreaseValue / value1) * 100
+  const result = (decreaseValue / value1) * 100;
   return isNaN(result) ? "0.00" : result.toFixed(2);
-}
+};
