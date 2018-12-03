5857ea0fe36e1d44b836f8fb81a6d02b4cccd287
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-12-03 11:48:44 +0100

    Add graph lines for max output

--- src/client/containers/Home/components/CapacityGraph.tsx
+++ src/client/containers/Home/components/CapacityGraph.tsx
@@ -7,10 +7,12 @@ import {
   VictoryArea,
   VictoryAxis,
   VictoryChart,
+  VictoryLabel,
+  VictoryLine,
   VictoryTooltip
 } from "victory";
 
-import { formatBytes, formatTime } from "../utils";
+import { findMaxValue, formatBytes, formatTime } from "../utils";
 import { CapacityTooltip, CapacityZoomChart } from "./";
 
 const AXIS: { x: string; y: string } = {
@@ -46,7 +48,10 @@ class CapacityGraph extends Component<CapacityGraphProps, CapacityGraphState> {
     const { cdn, p2p } = this.props;
     const { selectedDomain } = this.state;
 
-    const formatAxisBytes = (bytes: number) => formatBytes(bytes) + "\nGbps"
+    const maxCDN = findMaxValue(cdn);
+    const maxP2P = findMaxValue(p2p);
+
+    const formatAxisBytes = (bytes: number) => formatBytes(bytes) + "\nGbps";
 
     const VictoryZoomVoronoiContainer = createContainer("zoom", "voronoi");
 
@@ -88,6 +93,35 @@ class CapacityGraph extends Component<CapacityGraphProps, CapacityGraphState> {
             y={AXIS.y}
             style={{
               data: { fill: "#B2125C", stroke: "#511883", strokeWidth: 3 }
+            }}
+          />
+          <VictoryLine
+            data={[
+              { x: cdn[0].date, y: maxCDN },
+              { x: cdn[cdn.length - 1].date, y: maxCDN }
+            ]}
+            labels={[`Maximum CDN contribution: ${formatBytes(maxCDN)}Gbps`]}
+            labelComponent={<VictoryLabel x={175} />}
+            style={{
+              data: {
+                stroke: "#B2125C",
+                strokeDasharray: "5,5",
+              }
+            }}
+          />
+          <VictoryLine
+            data={[
+              { x: cdn[0].date, y: maxP2P },
+              { x: cdn[cdn.length - 1].date, y: maxP2P }
+            ]}
+            labels={[`Maximum throughput: ${formatBytes(maxP2P)}Gbps`]}
+            labelComponent={<VictoryLabel y={80} x={600} />}
+            style={{
+              data: {
+                stroke: "#458741",
+                strokeDasharray: "5,5",
+                strokeWidth: 3
+              }
             }}
           />
         </VictoryChart>
