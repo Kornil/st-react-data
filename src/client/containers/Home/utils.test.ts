a3aae2f603fbccaee35e47a8eda6140fde13d3b5
Francesco Agnoletto <francesco.agnoletto92@gmail.com>
2018-12-03 12:24:32 +0100

    Test findRightData corret behaviour

--- src/client/containers/Home/utils.test.ts
+++ src/client/containers/Home/utils.test.ts
@@ -1,13 +1,29 @@
 import { findRightData, getPercentage } from "./utils";
 
+it("findRightData returns correct vaue if right params are provided", () => {
+  const date = new Date();
+  expect(
+    findRightData(
+      date,
+      [{ date, gbps: 123 }, { date: new Date(), gbps: 111 }],
+      [{ date, gbps: 345 }, { date: new Date(), gbps: 222 }]
+    )
+  ).toEqual({
+    cdn: { date, gbps: 123 },
+    p2p: { date, gbps: 345 }
+  });
+});
+
 it("findRightData returns null if first value is undefined", () => {
   const date = new Date();
-  expect(findRightData(undefined, [{date, gbps: 123}], [{date, gbps: 123}])).toBeNull()
-})
+  expect(
+    findRightData(undefined, [{ date, gbps: 123 }], [{ date, gbps: 123 }])
+  ).toBeNull();
+});
 
 it("getPercentage returns right %", () => {
   const a = 100;
   const b = 50;
 
-  expect(getPercentage(a,b)).toBe("50.00");
-})
+  expect(getPercentage(a, b)).toBe("50.00");
+});
